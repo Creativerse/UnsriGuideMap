64cdd59a03afd95ad155f878bc615e8454a027cd
hasbyalkaff <hasbyalkaff@gmail.com>
2015-12-05 13:36:44 +0700

    add activity

--- app/src/main/java/mobcom/ilkom/unsri/unsriguidemap/SplashActivity.java
+++ app/src/main/java/mobcom/ilkom/unsri/unsriguidemap/SplashActivity.java
@@ -1,11 +1,14 @@
 package mobcom.ilkom.unsri.unsriguidemap;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,25 +16,8 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_splash, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void masuk(View view){
+        Intent masukIntent = new Intent(this, MapActivity.class);
+        startActivity(masukIntent);
     }
 }
